75a4f80e58e5d7c9ba323afe82c97b7290e35ba0
zahide sarp <sarpzahide@gmail.com>
2023-08-14 23:34:35 +0300

    DOM-3 & DOM-4

--- day05/js/19-dom1.js
+++ day05/js/19-dom1.js
@@ -22,7 +22,10 @@ console.log(h1El.innerText);
 console.log(h1El.innerHTML);
 
 h1El.innerText = `<span>hello</span>`;
-h1El.innerHTML = `<span style="color:red">hello</span>`;
+h1El.innerHTML = `<span style="color:red; display:none">hello</span>`;
+console.log(h1El.innerText);
+console.log(h1El.innerHTML);
+console.log(h1El.textContent);
 
 menuItem3.innerText = "Yeni icerik";
 
@@ -33,5 +36,8 @@ document.querySelector("ul.menu li:last-child").innerText += ` ${menuItem3Text}`
 document.querySelector("ul.menu li:nth-child(4)").innerText = "<b>Hello World</b>";
 
 document.querySelector("ul.menu li:nth-child(4)").innerHTML = "<b>Hello World</b>";
+
+
+
 
 
