a25882c8a6c8796ffc15656c23ba8e12598622d7
zahide sarp <sarpzahide@gmail.com>
2023-08-15 22:39:29 +0300

    if-practice2

--- day06/js/24-if-practice1.js
+++ day06/js/24-if-practice1.js
@@ -1,11 +1,11 @@
 /* 
-​
+
 1 - sayfada bir input olacak
 2 - inputa bir fiyat bilgisi girilecek
 3 - inputun yaninda bir buton olacak ve bu butona tiklandiginda asagidaki fonksiyonu calistiracak
 4 - fiyat bilgisi 500'den buyukse 10% indirim uygulayan degilse de herhangi bir degisiklik yapmayan fonksiyon yazilacak
 5 - ciktiyi sayfada bir elementin icerisinde gosterecek
-​
+
 */
 
 function calculate() {
