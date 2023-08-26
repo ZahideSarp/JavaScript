f403da6c060803f1b7fcaffaba322786c19dfdc1
zahide sarp <sarpzahide@gmail.com>
2023-08-26 20:51:23 +0300

    PIPELİNE

--- day14/js/odev.js
+++ day14/js/odev.js
@@ -4,25 +4,70 @@ const btnSortEl=document.querySelector("#sort");
 const btnReverseEl=document.querySelector("#reverse");
 const btnRemoveLastEl=document.querySelector("#removeLast");
 const btnRemoveFirstEl=document.querySelector("#removeFirst");
-const btnRemoveAlEl=document.querySelector("#removeAll");
+const btnRemoveAllEl=document.querySelector("#removeAll");
 const btnMixEl=document.querySelector("#mix");
 const arrListEl=document.querySelector("#arrList");
+var cityListEl=document.getElementById("cityList");
+const inputEl=document.querySelector("#text");
 
- var sehirler=[];
+
+const sehirler=[];
+
+
 addEl.addEventListener("click", () => {
-   const textEl=document.querySelector("#text").value;
-  
-    sehirler.push(textEl) ;
-    arrListEl.innerText=`${sehirler}`
-    textEl.innerText="";
+   sehirler.push(inputEl.value);
+    inputEl.value="";
+arr(sehirler);
+  }); 
 
-});
+  const arr= (sehirler) => {
+  
+    cityListEl.innerHTML="";
+   for(var i=0; i<sehirler.length; i++){
+   //cityListEl.innerHTML = "<li>"+ sehirler[i] +"</li>";
+   var liElemt=document.createElement("li");
+   liElemt.textContent=sehirler[i];
+   cityListEl.appendChild(liElemt);
+}
+  };
+
 
     
     btnSortEl.addEventListener("click", () => {
-        sehirler.sort;
+    sehirler.sort();
+    arr(sehirler);
+    }); 
+
+    btnReverseEl.addEventListener("click", () =>{
+    sehirler.reverse();
+    arr(sehirler);
     });
 
+    btnRemoveLastEl.addEventListener("click", () =>{
+       sehirler.pop();
+       arr(sehirler);
+
+    });
+
+btnRemoveFirstEl.addEventListener("click", () =>{
+sehirler.shift();
+
+});
+
+btnRemoveAllEl.addEventListener("click", () =>{
+sehirler.splice(0,sehirler.length);
+arr(sehirler);
+});
+
+btnMixEl.addEventListener("click", () => {
+   for (var i = sehirler.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = sehirler[i];
+      sehirler[i] = sehirler[j];
+      sehirler[j] = temp;
+  }
+  arr(sehirler);
+});
 
 
 
